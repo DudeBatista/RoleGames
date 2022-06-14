2b1a6037bf6f0a30cb6e25070d4e66f9547eb1a2
DudeBatista <maria.santos1894@etec.sp.gov.br>
2022-06-14 17:14:16 -0300

    Arrumando nome do app e logo

--- app/src/main/java/com/example/rolegames/Login.java
+++ app/src/main/java/com/example/rolegames/Login.java
@@ -2,13 +2,26 @@ package com.example.rolegames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    public Button btncad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+    }
+    public void IrPraCad(View view){
+
+        findViewById(R.id.btncad);
+        Intent intent =  new Intent(Login.this, Cadastro.class);
+        startActivity(intent);
     }
 }
