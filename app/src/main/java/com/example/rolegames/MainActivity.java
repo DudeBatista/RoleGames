69e7dd75a95e4ef1ed17e5e7373328e6e8790bb3
DudeBatista <maria.santos1894@etec.sp.gov.br>
2022-06-14 20:38:52 -0300

    tentando

--- app/src/main/java/com/example/rolegames/MainActivity.java
+++ app/src/main/java/com/example/rolegames/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.rolegames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-public class MainActivity extends AppCompatActivity {
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private LinearLayout lineNoticias;
+    String query = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    lineNoticias = (LinearLayout) findViewById(R.id.LinearNoticias);
+
+        Intent intent = getIntent();
+        query = intent.getStringExtra("query");
+        Bundle queryBundle =  new Bundle();
+        queryBundle.putString("query", query);
+        if(getSupportLoaderManager().getLoader(0) != null){
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
+
+        fetchSteam();
+
+    }
+
+    public void fetchSteam(){
 
     }
 }
